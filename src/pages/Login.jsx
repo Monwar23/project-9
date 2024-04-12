dd4778b65843b6c28dd61ec5271997e805a419ac
monwar23 <monwar2345@gmail.com>
2024-04-13 02:40:47 +0600

    login page create

---
+++ src/pages/Login.jsx
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../firebase/FirebaseProvider";
+
+
+const Login = () => {
+
+    const { signIn } = useContext(AuthContext)
+
+    const handleLogin = e => {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const email = form.get('email')
+        const password = form.get('password')
+
+
+        signIn(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <div>
+            <div>
+                <h2 className="text-center text-3xl my-10">Please Login</h2>
+
+                <form onSubmit={handleLogin} className="md:3/4 lg:w-1/2 mx-auto">
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+                <p className="text-center mt-4">Do not have an Account ? Please <Link className="font-bold text-blue-600" to='/register'>Register</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
