cbd632363cb92b21fbf352c8dece84300787746e
monwar23 <monwar2345@gmail.com>
2024-04-12 22:58:14 +0600

    navbar completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
+// import { Link } from "react-router-dom";
 
 
 const Footer = () => {
     return (
-        <div>
-            <h2>footer</h2>
-        </div>
+      <div>
+        <h2>footer</h2>
+      </div>
     );
 };
 
