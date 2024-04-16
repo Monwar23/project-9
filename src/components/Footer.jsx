2a4f4566de17e9d56269111228391265c8f4c3cd
monwar23 <monwar2345@gmail.com>
2024-04-16 17:31:21 +0600

    added footer social icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 import { FaHouse } from "react-icons/fa6";
+import { FaFacebookF, FaInstagram, FaLinkedin, FaTwitter, } from "react-icons/fa";
+
 
 
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-blue-200 text-base-content mt-20">
             <aside>
-              <FaHouse className="text-6xl text-blue-600"></FaHouse>
+                <FaHouse className="text-6xl text-blue-600"></FaHouse>
                 <Link to="/" className=" text-xl font-medium"><span className="font-bold text-blue-600">Home</span>Spotter</Link>
+               <div className="flex gap-2">
+               <FaFacebookF className="text-lg"></FaFacebookF>
+                <FaTwitter className="text-lg"></FaTwitter>
+                <FaInstagram className="text-lg"></FaInstagram>
+                <FaLinkedin className="text-lg"></FaLinkedin>
+               </div>
             </aside>
             <nav className="text-black">
                 <h6 className="footer-title">ADVERTISERS</h6>
