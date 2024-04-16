cd772bff05a70cd5ea700ee9ff9f2d5a84390562
monwar23 <monwar2345@gmail.com>
2024-04-16 16:41:11 +0600

    extra one section completed

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # HomeSpotter
 
 
+# Features 
 
+- First item
+- Second item
+  - Subitem 1
+  - Subitem 2
+- Third item
--- src/components/MillionBuyers.jsx
+++ src/components/MillionBuyers.jsx
@@ -3,16 +3,37 @@ import { Link } from "react-router-dom";
 const MillionBuyers = () => {
     return (
         <div>
-           <div className="mt-10">
-           <h2 className="text-4xl font-bold text-center mt-5">Trusted By 100 Million Buyers</h2>
-           <p className="text-center mt-4">Only HomeSpotter connects you directly to the person that knows the most about a property for sale, the listing agent.</p>
-           </div>
-           <div>
-                <Link>
-                    <h2>Explore Great Neighborhoods</h2>
-                    <p>Explore video tours ,in-depth research,and article on 20,000 neighborhoods</p>
+            <div className="my-10">
+                <h2 className="text-4xl font-bold text-center mt-5">Trusted By 100 Million Buyers</h2>
+                <p className="text-center mt-4">Only HomeSpotter connects you directly to the person that knows the most about a property for sale, the listing agent.</p>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <Link className="flex flex-col">
+                    <div className="border p-14 rounded-lg bg-zinc-600 bg-gradient-to-r from-purple-500 to-pink-500 text-white flex-grow">
+                        <h2 className="text-2xl font-semibold mb-2">Explore Great Neighborhoods</h2>
+                        <p>Explore video tours, in-depth research, and articles on 20,000 neighborhoods</p>
+                    </div>
                 </Link>
-           </div>
+                <Link className="flex flex-col">
+                    <div className="border p-14 rounded-lg bg-zinc-600 bg-gradient-to-r from-purple-500 to-pink-500 text-white flex-grow">
+                        <h2 className="text-2xl font-semibold mb-2">Find Highly Rated Schools</h2>
+                        <p>Find the very best schools with in-depth reviews and ratings from multiple experts.</p>
+                    </div>
+                </Link>
+                <Link className="flex flex-col">
+                    <div className="border p-14 rounded-lg bg-zinc-600 bg-gradient-to-r from-purple-500 to-pink-500 text-white flex-grow">
+                        <h2 className="text-2xl font-semibold mb-2">Discover Residential Buildings</h2>
+                        <p>Discover residential buildings with our comprehensive building search.</p>
+                    </div>
+                </Link>
+                <Link className="flex flex-col">
+                    <div className="border p-14 rounded-lg bg-zinc-600 bg-gradient-to-r from-purple-500 to-pink-500 text-white flex-grow">
+                        <h2 className="text-2xl font-semibold mb-2">Find the Best Agent for You</h2>
+                        <p>Search a million agent profiles with bios, photographs, reviews, and deal history.</p>
+                    </div>
+                </Link>
+            </div>
+
         </div>
     );
 };
